ff7f5abcb89c64ef245c1d4e17550164008d44fc
Emboliii <anthonyiannini@gmail.com>
2025-07-15 16:00:33 +0200

    Fix: modificato file sw per aggiornamento veloce dell'app

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,27 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
     );
 
 });
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if(key !== CACHE_NAME){
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    clients.claim();
+})
 
 self.addEventListener('fetch', event => {
     event.respondWith(
